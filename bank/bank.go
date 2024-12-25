6b3841e06c3a12198be19a5c68b3fbe395185930
SiddhantMishra4 <sid@neuralix.ai>
2024-12-26 00:12:12 +0530

    55. Splitting code accross files in the same package

--- bank/bank.go
+++ bank/bank.go
@@ -44,12 +44,7 @@ func main() {
 	writeBalanceToFile(account_balance)
 	fmt.Println("Welcome to Go Bank!")
 	for {
-
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit Money")
-		fmt.Println("3. Withdraw Money")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		var choice int
 		fmt.Print("Your choice: ")
