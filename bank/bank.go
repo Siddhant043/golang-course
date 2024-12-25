66f3aa3c7f5b3427696b0e34b55649963a5f6e62
SiddhantMishra4 <sid@neuralix.ai>
2024-12-25 18:12:28 +0530

    48. Error handeling added

--- bank/bank.go
+++ bank/bank.go
@@ -1,18 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 )
+
+const accountBalanceFile string = "balance.txt"
+
+func readBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file.")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored balance value")
+	}
+
+	return balance, nil
+}
 
 func writeBalanceToFile(balance float64) {
 	balanceStr := fmt.Sprint(balance)
-	os.WriteFile("balance.txt", []byte(balanceStr), 0644)
+	os.WriteFile(accountBalanceFile, []byte(balanceStr), 0644)
 }
 
 func main() {
 
-	var account_balance = 1000.0
+	account_balance, err := readBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR!")
+		fmt.Println(err)
+		fmt.Println("----------------")
+	}
 	writeBalanceToFile(account_balance)
 	fmt.Println("Welcome to Go Bank!")
 	for {
