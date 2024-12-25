30531ca4b2027d21a549453cad9cbc06819f0d6a
SiddhantMishra4 <sid@neuralix.ai>
2024-12-25 23:48:36 +0530

    52. Exercise solution

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func checkValidInput(num float64) error {
+	if num <= 0 {
+		return errors.New("Please enter a value larger than 0")
+	}
+	return nil
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
 
 func main() {
 	revenue := getUserInput("Revenue: ")
@@ -11,24 +27,26 @@ func main() {
 
 	ebt, profit, ratio := calcFinancials(revenue, expences, taxRate)
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
-
+	fmt.Printf("%0.2f\n", ebt)
+	fmt.Printf("%0.2f\n", profit)
+	fmt.Printf("%0.2f\n", ratio)
+	storeResults(ebt, profit, ratio)
 }
 
 func getUserInput(infoText string) float64 {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
+	err := checkValidInput(userInput)
+	if err != nil {
+		panic(err)
+	}
 	return userInput
 }
 
 func calcFinancials(revenue, expences, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expences
-
 	profit := ebt * (1 - taxRate/100)
-
 	ratio := ebt / profit
 
 	return ebt, profit, ratio
