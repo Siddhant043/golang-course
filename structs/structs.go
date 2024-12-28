f9e18fd48ad51300f639c1ac53d80bc6851bbdb5
SiddhantMishra4 <sid@neuralix.ai>
2024-12-28 23:24:19 +0530

    81. Introducing methods

--- structs/structs.go
+++ structs/structs.go
@@ -8,24 +8,22 @@ type user struct {
 	birthdate string
 }
 
+func (u user) outputUserDetails() {
+	// this is a shortcut provided by go, technically it is (*u).firstName), you have to dereference it normally
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your First Name: ")
 	userLastName := getUserData("Please enter your Last Name: ")
 	userBirthdate := getUserData("Please enter your Birth Date (MM/DD/YYY): ")
 
-	var appUser user
-
-	appUser = user{
+	appUser := user{
 		firstName: userFirstName,
 		lastName:  userLastName,
 		birthdate: userBirthdate,
 	}
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) {
-	// this is a shortcut provided by go, technically it is (*u).firstName), you have to dereference it normally
-	fmt.Println(u.firstName, u.lastName, u.birthdate)
+	appUser.outputUserDetails()
 }
 
 func getUserData(msg string) string {
