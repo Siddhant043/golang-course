627cc21ea9d04132776428b2a1124c654eefa58e
SiddhantMishra4 <sid@neuralix.ai>
2024-12-31 20:57:06 +0530

    117. Array and slice exercise completed

--- arrays/main.go
+++ arrays/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+type Product struct {
+	title string
+	id    string
+	price float64
+}
+
 func main() {
 	//slicesExample()
-	dynamicArrayExample()
+	//dynamicArrayExample()
+	arrayPractice()
 }
 
 func slicesExample() {
@@ -35,4 +42,46 @@ func dynamicArrayExample() {
 
 	prices = append(prices, 5.99)
 	fmt.Println(prices)
+}
+
+func arrayPractice() {
+	hobbies := [3]string{"Coding", "Riding", "Gaming"}
+	// 1
+	fmt.Println("Hobbies list: ", hobbies)
+
+	// 2
+	fmt.Println("First Element: ", hobbies[0])
+	fmt.Println("Second and Third Element: ", hobbies[1:3])
+
+	// 3
+	firstTwoElements := hobbies[:2]
+	fmt.Println("First and Second Element: ", hobbies[:2])
+	fmt.Println("First and Second Element: ", hobbies[0:2])
+
+	// 4
+
+	firstTwoElements = firstTwoElements[1:3]
+	fmt.Println("Second and Last Element: ", firstTwoElements)
+
+	//5
+
+	goalsArr := []string{}
+
+	goalsArr = append(goalsArr, "learning")
+	goalsArr = append(goalsArr, "personalGrowth")
+	fmt.Println("Goals array: ", goalsArr)
+
+	// 6
+	goalsArr[1] = "personalExperience"
+	goalsArr = append(goalsArr, "careerGrowth")
+	fmt.Println("Updated Goals array: ", goalsArr)
+
+	// 7
+	productsArr := []Product{{id: "phone1", title: "Phone 1", price: 22.60}, {id: "phone2", title: "Phone 2", price: 40.60}}
+
+	fmt.Println("Products: ", productsArr)
+
+	newProduct := Product{id: "phone3", title: "Phone 3", price: 99.10}
+	productsArr = append(productsArr, newProduct)
+	fmt.Println("Updated Products: ", productsArr)
 }
